d0a29818372ffdcf456acce657c36e0c9ba92661
premsai-20 <sakamurisai252@gmail.com>
2025-08-01 14:01:38 +0530

    1.Bean Scope 2.Section 9-57

--- learn-spring-framework-02/src/main/java/com/premsai/learn_spring_framework/example/a0/RealWorldSpringContextLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/premsai/learn_spring_framework/example/a0/RealWorldSpringContextLauncherApplication.java
@@ -1,7 +1,5 @@
 package com.premsai.learn_spring_framework.example.a0;
 
-import java.util.Arrays;
-
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -15,13 +13,8 @@ public class RealWorldSpringContextLauncherApplication {
 	public static void main(String[] args) {
 		
 		try(var context = new AnnotationConfigApplicationContext(
-				RealWorldSpringContextLauncherApplication.class)){
-			Arrays.stream(context.getBeanDefinitionNames())
-			.forEach(System.out::println);
-			
-			System.out.println(context.getBean(BusinessCalculationService.class).findMax());
-			}
-		
-	}
+				RealWorldSpringContextLauncherApplication.class)){		
+		}
 
+	}
 }
